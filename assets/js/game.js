3279c45d17f0bf61e2c0a6104e379913b2af82ac
James Killelea <dylan.killelea@yahoo.com>
2022-01-05 13:17:43 -0600

    added enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,11 +5,21 @@ var playerMoney = 10;
 
 //Enemy info
 
-var enemyName ="Roberto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
+console.log(enemyNames);
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames.length);
+for (var i = 0; i < enemyNames.length; i++) {
+  console.log(enemyNames[i]);
+  console.log(i);
+  console.log(enemyNames[i] + " is at " + i + " index");
+}
 
-var fight = function() {
+var fight = function(enemyName) {
   //Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
@@ -63,4 +73,6 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   window.alert("You need to choose a valid option. Try again!");
 }
 };
-fight();
+for (var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
